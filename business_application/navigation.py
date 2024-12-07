1d7ac72d16cf8f8015c39ae7675851d776ed7309
network <imitruk@redhat.com>
2024-12-07 19:38:04 +0100

    misc: update AI disclaimer

--- business_application/navigation.py
+++ business_application/navigation.py
@@ -1,11 +1,38 @@
-from extras.plugins import PluginNavigationMenu
+from django.conf import settings
+from netbox.plugins import PluginMenuItem, PluginMenuButton
 
-navigation_menu = PluginNavigationMenu(
-    items=[
-        {
-            'name': 'Business Applications',
-            'url': 'plugins:business_application:businessapplication_list',
-            'icon_class': 'mdi mdi-apps',
-        },
-    ]
-)
+# compatibility with netbox v3.3 that does not have PluginMenu
+try:
+    from netbox.plugins import PluginMenu
+    HAVE_MENU = True
+except ImportError:
+    HAVE_MENU = False
+    PluginMenu = PluginMenuItem
+
+
+app_buttons = [
+    PluginMenuItem(
+        link='plugins:business_application:businessapplication_list',
+        link_text='Business Applications',
+        permissions=['business_application.view_businessapplication'],
+        buttons=[
+            PluginMenuItem(
+                link='plugins:business_application:businessapplication_add',
+                link_text='Add Business Application',
+                permissions=['business_application.add_businessapplication'],
+            ),
+        ]
+    )
+]
+if (HAVE_MENU and settings.PLUGINS_CONFIG['netbox_inventory']['top_level_menu']):
+    # add a top level entry
+    menu = PluginMenu(
+        label=f'Bussiness Applications',
+        groups=(
+            ('Applications', app_buttons),
+        ),
+        icon_class='mdi mdi-clipboard-text-multiple-outline'
+    )
+else:
+    # display under plugins
+    menu_items = app_buttons
