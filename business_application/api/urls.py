6a458588b3cfc0ad55f6a585705c3e16c67db9d8
carolcodetest <carolcodetest@gmail.com>
2025-05-14 13:37:42 +0800

    Update urls.py

--- business_application/api/urls.py
+++ business_application/api/urls.py
@@ -7,6 +7,20 @@ router.register(r'business-applications', BusinessApplicationViewSet)
 router.register(r'devices', DeviceDownstreamAppsViewSet, basename='device-downstream-apps')
 router.register(r'clusters', ClusterDownstreamAppsViewSet, basename='cluster-downstream-apps')
 
-urlpatterns = [
-    path('', include(router.urls)),
+urlpatterns = router.urls + [
+    path(
+        'devices/downstream-applications/<int:pk>/',
+        DeviceDownstreamAppsViewSet.as_view({'get': 'retrieve'}),
+        name='device-downstream-applications-detail'
+    ),
+    path(
+        'devices/downstream-applications/',
+        DeviceDownstreamAppsViewSet.as_view({'get': 'list'}),
+        name='device-downstream-applications-list'
+    ),
+    path(
+        'clusters/downstream-applications/<int:pk>/',
+        ClusterDownstreamAppsViewSet.as_view({'get': 'retrieve'}),
+        name='cluster-downstream-applications-detail'
+    ),
 ]
